6d24720b8af6d19917543ab29ff90b13d8b2ff93
davidmukiibi <david.mukiibiq@gmail.com>
2017-07-08 21:05:18 +0300

    [Chore] Add instance file that configures the environments.

---
+++ instance/config.py
@@ -0,0 +1,39 @@
+import os
+from decouple import config
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = config('DEV_SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config('DEV_SECRET_KEY')
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = config('TESTING_SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('TESTING_SQLALCHEMY_TRACK_MODIFICATIONS')
+    DEBUG = config('TESTING_DEBUG')
+    SERVER_NAME = config('TESTING_SERVER_NAME')
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
