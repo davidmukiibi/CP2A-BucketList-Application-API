3c9e1f35cff9f79143e5c7f7bc2893db0c5de422
davidmukiibi <david.mukiibiq@gmail.com>
2017-07-08 21:03:55 +0300

    [Chore] Code Refactor on the routes.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from buckeT import app, api
-from buckeT.bucketlist import RegisterUser, LoginUser
+from buckeT.bucketlist import RegisterUser, LoginUser, Bucketlist, BucketlistItem, SingleBucketlist, SingleBucketlistItem
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
@@ -8,7 +8,10 @@ config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
 api.add_resource(RegisterUser, '/auth/register/')
 api.add_resource(LoginUser, '/auth/login/')
-
+api.add_resource(Bucketlist, '/bucketlists/')
+api.add_resource(SingleBucketlist, '/bucketlists/<int:ID>')
+api.add_resource(BucketlistItem, '/bucketlists/<int:ID>/items/')
+api.add_resource(SingleBucketlistItem, '/bucketlists/<int:ID_bucket>/items/<int:ID_item>')
 
 if __name__ == '__main__':
     app.run()
