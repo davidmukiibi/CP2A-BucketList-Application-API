b882f1a05fe5b9fe70cb67439daef14d8c34b972
davidmukiibi <david.mukiibiq@gmail.com>
2017-07-11 23:25:22 +0300

    [Chore] Fix server name error

--- instance/config.py
+++ instance/config.py
@@ -20,7 +20,7 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     DEBUG = True
-    SERVER_NAME = 127.0.0.1:5000
+    SERVER_NAME = '127.0.0.1:5000'
 
 class StagingConfig(Config):
     """Configurations for Staging."""
