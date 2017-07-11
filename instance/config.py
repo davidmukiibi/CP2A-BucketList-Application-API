33638f676bc3c4cb1c8b40b95d5332abf23765f2
davidmukiibi <david.mukiibiq@gmail.com>
2017-07-11 23:03:40 +0300

    [Chore] Sensitive data abstraction.

--- instance/config.py
+++ instance/config.py
@@ -11,16 +11,16 @@ class Config(object):
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jamaica'
-    SECRET_KEY = 'thisisthesecretkey'
+    SQLALCHEMY_DATABASE_URI = config('DEV_SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config('DEV_SECRET_KEY')
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    DEBUG = True
-    SERVER_NAME = '127.0.0.1:5000'
+    TESTING = os.getenv('TESTING')
+    SQLALCHEMY_DATABASE_URI = os.getenv('TESTING_SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('TESTING_SQLALCHEMY_TRACK_MODIFICATIONS')
+    DEBUG = os.getenv('TESTING_DEBUG')
+    SERVER_NAME = os.getenv('TESTING_SERVER_NAME')
 
 class StagingConfig(Config):
     """Configurations for Staging."""
